4b9a52ea48ee2673d0b3da977f7b630881fc4ba1
Ali Nahm <alisnahm@gmail.com>
2013-07-27 17:25:43 -0700

    bleh

---
+++ models.py
@@ -0,0 +1,17 @@
+# db
+
+from hello import db
+
+class Sentence(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    gameId = db.Column(db.Integer, unique=True)
+    orderId = db.Column(db.Integer, unique=True)
+    text = db.Column(db.String(120), unique=True)
+
+    def __init__(self, gameId, orderId, text):
+        self.gameId = gameId
+        self.orderId = orderId
+        self.text = text
+
+    def __repr__(self):
+        return '<User %r>' % self.text
