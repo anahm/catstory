a7aa4692c3b0282c1335a5bd560cc152a1d9ae19
Ali Nahm <alisnahm@gmail.com>
2013-07-27 17:47:43 -0700

    once more

--- hello.py
+++ hello.py
@@ -28,16 +28,4 @@ def start():
 # def join(username, gameId):
 
 
-class Sentence(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    gameId = db.Column(db.Integer, unique=True)
-    orderId = db.Column(db.Integer, unique=True)
-    text = db.Column(db.String(120), unique=True)
 
-    def __init__(self, gameId, orderId, text):
-        self.gameId = gameId
-        self.orderId = orderId
-        self.text = text
-
-    def __repr__(self):
-        return '<User %r>' % self.text
