770ddc1998909f9c3407ab0a8f24127cecec15ee
Ali Nahm <alisnahm@gmail.com>
2013-07-27 17:11:13 -0700

    db boilerplate?

--- hello.py
+++ hello.py
@@ -1,8 +1,19 @@
 import os
 from flask import Flask
 
+# db boilerplate code?
+from flask.ext.sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+db = SQLAlchemy(app)
+
 
 @app.route('/')
 def hello():
     return 'Hello World!'
+
+
+
+
